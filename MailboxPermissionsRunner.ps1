67bc64616f890f7203c2e827deb7c64571d50f77
afernandez1 <afernandez1@users.noreply.github.com>
2024-08-13 23:12:38 -0700

    Update MailboxPermissionsRunner.ps1

--- MailboxPermissionsRunner.ps1
+++ MailboxPermissionsRunner.ps1
@@ -5,10 +5,10 @@
 Connect-ExchangeOnline
 
 #Prompt the user for the mailbox address that will be accessed
-$mailbox = Read-Host "Enter mailbox address that will be accessed:"
+$mailbox = Read-Host "Enter mailbox address that will be accessed"
 
 #Prompt the user for the email address of the user who needs access the mailbox
-$username = Read-Host "Enter email address of the user who needs access:"
+$username = Read-Host "Enter email address of the user who needs access"
 
 #Unmap the mailbox from Outlook by removing Full Access
 Remove-MailboxPermission -Identity $mailbox -User $username -AccessRights FullAccess
