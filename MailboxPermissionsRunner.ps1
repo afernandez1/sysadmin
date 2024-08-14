57632ddd33e71d68c975e2593009afb691764e55
afernandez1 <afernandez1@users.noreply.github.com>
2024-08-13 23:09:40 -0700

    Update MailboxPermissionsRunner.ps1

--- MailboxPermissionsRunner.ps1
+++ MailboxPermissionsRunner.ps1
@@ -1,17 +1,20 @@
-#This script prompts for a mailbox and a user and then removes
-#the user's permissions from the mailbox and then re-adds the
-#permissions without mapping the mailbox to Outlook
+#Use this script to delegate access to a mailbox without
+#mapping the mailbox to Outlook.
 
+#Connect to Exchange Online
 Connect-ExchangeOnline
 
-#Prompt the user to enter email address
-$mailbox = Read-Host "Enter mailbox address to be accessed"
+#Prompt the user for the mailbox address that will be accessed
+$mailbox = Read-Host "Enter mailbox address that will be accessed:"
 
-#Prompt the user to enter email address
-$username = Read-Host "Enter email address of user who needs access"
+#Prompt the user for the email address of the user who needs access the mailbox
+$username = Read-Host "Enter email address of the user who needs access:"
 
+#Unmap the mailbox from Outlook by removing Full Access
 Remove-MailboxPermission -Identity $mailbox -User $username -AccessRights FullAccess
 
+#Delegate full access to the mailbox without mapping the mailbox to Outlook
 Add-MailboxPermission -Identity $mailbox -User $username -AccessRights FullAccess -AutoMapping $false
 
+#Disconnect from Exchange Online
 Disconnect-ExchangeOnline -Confirm:$false
